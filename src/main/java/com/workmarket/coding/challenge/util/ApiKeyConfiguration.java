07f1b3a3595090a1d81764534b4fc314d9c8cada
victorobiahu07 <vick.obiahu@gmail.com>
2019-10-22 13:27:31 -0400

    Update ApiKeyConfiguration.java

--- src/main/java/com/workmarket/coding/challenge/util/ApiKeyConfiguration.java
+++ src/main/java/com/workmarket/coding/challenge/util/ApiKeyConfiguration.java
@@ -1,6 +1,7 @@
 package com.workmarket.coding.challenge.util;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
@@ -8,7 +9,9 @@ import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 
 @Configuration
+@EnableZuulProxy
 @PropertySource("classpath:application.properties")
+
 public class ApiKeyConfiguration {
 
 	@Value("${apiKey}")
